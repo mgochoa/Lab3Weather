bca5c75ef975e714e33b07384185b15e18e149c3
mgochoa <mochoa1127@gmail.com>
2016-09-08 10:15:24 -0500

    Gson for weather request

--- app/src/main/java/co/edu/udea/compumovil/gr3/lab3weather/Fragments/weather.java
+++ app/src/main/java/co/edu/udea/compumovil/gr3/lab3weather/Fragments/weather.java
@@ -4,6 +4,7 @@ package co.edu.udea.compumovil.gr3.lab3weather.Fragments;
 import android.app.DownloadManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +15,14 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
+import org.json.JSONObject;
+
+import co.edu.udea.compumovil.gr3.lab3weather.POJO.weatherPOJO;
 import co.edu.udea.compumovil.gr3.lab3weather.R;
 
 /**
@@ -25,7 +31,9 @@ import co.edu.udea.compumovil.gr3.lab3weather.R;
 public class weather extends Fragment {
 
     Button getWeather;
-    TextView tvWeather;
+    TextView tvCiudad,tvTemp,tvHum,tvDescription,tvIcon;
+    Gson outGson;
+    weatherPOJO wp;
     private final String API_KEY="a114981a45d6ad13ade4e27c615513b9";
     public weather() {
         // Required empty public constructor
@@ -39,29 +47,44 @@ public class weather extends Fragment {
         View thisview=inflater.inflate(R.layout.fragment_weather, container, false);
         getActivity().setTitle("Clima");
         getWeather=(Button)thisview.findViewById(R.id.btn_weather);
-        tvWeather=(TextView)thisview.findViewById(R.id.tv_weather);
+        tvCiudad=(TextView)thisview.findViewById(R.id.tv_city);
+        tvTemp=(TextView)thisview.findViewById(R.id.tv_temp);
+        tvHum=(TextView)thisview.findViewById(R.id.tv_humidity);
+        tvDescription=(TextView)thisview.findViewById(R.id.tv_description);
+        tvIcon=(TextView)thisview.findViewById(R.id.tv_icon);
         getWeather.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 RequestQueue queue = Volley.newRequestQueue(getContext());
-                String url ="http://api.openweathermap.org/data/2.5/weather?q=Medellin,co&appid="+API_KEY;
+                String url ="http://api.openweathermap.org/data/2.5/weather?q=Medellin,co&appid="+API_KEY+"&lang=en&units=metric";
 
 // Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                        new Response.Listener<String>() {
+                JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
                             @Override
-                            public void onResponse(String response) {
-                                // Display the first 500 characters of the response string.
-                                tvWeather.setText("Response is: "+ response.substring(0,500));
+                            public void onResponse(JSONObject response) {
+                                //tvWeather.setText("Response: " + response.toString());
+                                outGson=new Gson();
+                                wp=outGson.fromJson(response.toString(),weatherPOJO.class);
+                                tvCiudad.setText(wp.getName());
+                                tvDescription.setText(wp.getWeather().get(0).getDescription());
+                                tvTemp.setText(Integer.toString(wp.getMain().getTemp()));
+                                tvHum.setText(Integer.toString(wp.getMain().getHumidity()));
+                                tvIcon.setText(wp.getWeather().get(0).getIcon());
+                                Log.d("weather.java",wp.getName()+wp.getMain().getTemp()+wp.getMain().getHumidity()+wp.getWeather().get(0).getDescription()+wp.getWeather().get(0).getIcon());
+
                             }
                         }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        tvWeather.setText("That didn't work!");
-                    }
-                });
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // TODO Auto-generated method stub
+
+                            }
+                        });
 // Add the request to the RequestQueue.
-                queue.add(stringRequest);
+                queue.add(jsObjRequest);
             }
         });
         return thisview;
---
+++ app/src/main/java/co/edu/udea/compumovil/gr3/lab3weather/POJO/coord.java
@@ -0,0 +1,27 @@
+package co.edu.udea.compumovil.gr3.lab3weather.POJO;
+
+/**
+ * Created by mguillermo.ochoa on 8/09/16.
+ */
+public class coord {
+    float lat,longitude;
+
+    public coord() {
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+}
