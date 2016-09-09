6e5b22cf98ef734316cb3a69a3b39d9ae27ba31f
mgochoa <mochoa1127@gmail.com>
2016-09-09 15:21:30 -0500

    Retrieves icon and load it

--- app/src/main/java/co/edu/udea/compumovil/gr3/lab3weather/Fragments/weather.java
+++ app/src/main/java/co/edu/udea/compumovil/gr3/lab3weather/Fragments/weather.java
@@ -2,6 +2,7 @@ package co.edu.udea.compumovil.gr3.lab3weather.Fragments;
 
 
 import android.app.DownloadManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -9,21 +10,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 import org.json.JSONObject;
 
 import co.edu.udea.compumovil.gr3.lab3weather.POJO.weatherPOJO;
 import co.edu.udea.compumovil.gr3.lab3weather.R;
+import co.edu.udea.compumovil.gr3.lab3weather.Singleton.MySingleton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,10 +37,13 @@ import co.edu.udea.compumovil.gr3.lab3weather.R;
 public class weather extends Fragment {
 
     Button getWeather;
+    ImageView iconView;
     TextView tvCiudad,tvTemp,tvHum,tvDescription,tvIcon;
     Gson outGson;
     weatherPOJO wp;
+    ImageLoader imageLoader= ImageLoader.getInstance();
     private final String API_KEY="a114981a45d6ad13ade4e27c615513b9";
+
     public weather() {
         // Required empty public constructor
     }
@@ -52,11 +61,13 @@ public class weather extends Fragment {
         tvHum=(TextView)thisview.findViewById(R.id.tv_humidity);
         tvDescription=(TextView)thisview.findViewById(R.id.tv_description);
         tvIcon=(TextView)thisview.findViewById(R.id.tv_icon);
+        iconView=(ImageView)thisview.findViewById(R.id.icon_image);
         getWeather.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 RequestQueue queue = Volley.newRequestQueue(getContext());
                 String url ="http://api.openweathermap.org/data/2.5/weather?q=Medellin,co&appid="+API_KEY+"&lang=en&units=metric";
+                final String  urlImage="http://openweathermap.org/img/w/";
 
 // Request a string response from the provided URL.
                 JsonObjectRequest jsObjRequest = new JsonObjectRequest
@@ -72,6 +83,8 @@ public class weather extends Fragment {
                                 tvTemp.setText(Integer.toString(wp.getMain().getTemp()));
                                 tvHum.setText(Integer.toString(wp.getMain().getHumidity()));
                                 tvIcon.setText(wp.getWeather().get(0).getIcon());
+                                imageLoader.init(ImageLoaderConfiguration.createDefault(getContext()));
+                                imageLoader.displayImage(urlImage+wp.getWeather().get(0).getIcon()+".png",iconView);
                                 Log.d("weather.java",wp.getName()+wp.getMain().getTemp()+wp.getMain().getHumidity()+wp.getWeather().get(0).getDescription()+wp.getWeather().get(0).getIcon());
 
                             }
@@ -83,8 +96,26 @@ public class weather extends Fragment {
 
                             }
                         });
+                MySingleton.getInstance(getContext()).addToRequestQueue(jsObjRequest);
+               // Log.d("weather.java",wp.getWeather().get(0).getIcon());
+
+               /* ImageRequest request = new ImageRequest(urlImage,
+                        new Response.Listener<Bitmap>() {
+                            @Override
+                            public void onResponse(Bitmap bitmap) {
+                                iconView.setImageBitmap(bitmap);
+                            }
+                        }, 0, 0, null,
+                        new Response.ErrorListener() {
+                            public void onErrorResponse(VolleyError error) {
+                                iconView.setImageResource(R.drawable.ic_menu_camera);
+                            }
+                        });
 // Add the request to the RequestQueue.
-                queue.add(jsObjRequest);
+
+
+                MySingleton.getInstance(getContext()).addToRequestQueue(request);*/
+
             }
         });
         return thisview;
